280fcaa183762ae4724ce1f50f9a6b01126a487c
NataliaSalas <natalia.salas01@correo.usa.edu.co>
2022-11-22 15:12:06 -0500

    First Commit

--- src/module-info.java
+++ src/module-info.java
@@ -1,2 +1,3 @@
 module PollaMundialista {
+	requires java.desktop;
 }
---
+++ src/vista/Panel.java
@@ -0,0 +1,12 @@
+package vista;
+
+import javax.swing.JPanel;
+
+public class Panel extends JPanel{
+	public Panel() {
+		this.setSize(800,800);
+		this.setVisible(true);
+	}
+	
+
+}
