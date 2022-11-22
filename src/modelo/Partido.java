c0e67c29393073c0a485aed620c0847df73cd225
NataliaSalas <natalia.salas01@correo.usa.edu.co>
2022-11-22 14:23:17 -0500

    First Commit

--- src/modelo/Partido.java
+++ src/modelo/Partido.java
@@ -1,5 +1,28 @@
 package modelo;
 
 public class Partido {
+	Equipo e1;
+	Equipo e2;
+	
+	public Partido(Equipo eq1, Equipo eq2) {
+		this.e1=eq1;
+		this.e2=eq2;
+	}
+	
+	public Equipo getE1() {
+		return e1;
+	}
+
+	public void setE1(Equipo e1) {
+		this.e1 = e1;
+	}
+
+	public Equipo getE2() {
+		return e2;
+	}
+
+	public void setE2(Equipo e2) {
+		this.e2 = e2;
+	}
 
 }
